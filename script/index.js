76eb1d85618d11cf1c834e03d6632d2f653b277c
yunseoyoung <seoyoung6695@gamil.com>
2025-08-07 18:53:00 +0900

    수정

--- script/index.js
+++ script/index.js
@@ -56,28 +56,6 @@ const lookbookSwiper = new Swiper('#look_book',{
     },
     watchOverflow:true,
 });
-const gnb =document.querySelector('.gnb');
-const lnb =document.querySelector('.lnb_bg');
-gnb.addEventListener('mouseenter', ()=> {
-    lnb.style.display ='block';
-
-});
-
-gnb.addEventListener('mouseleave',( )=>{
-        lnb.style.display='none';
-
-});
-
-lnb.addEventListener('mouseenter',()=>{
-    
-        lnb.style.display= 'block';
-    
-});
-lnb.addEventListener('mouseleave',()=>{
-    
-        lnb.style.display= 'none';
-    
-});
 
 const categoryList = document.querySelectorAll('.category_list li');
 const swiperWraps = document.querySelectorAll('.best_items_wrap');
